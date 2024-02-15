d0c45e9e509d99303ca6301897a009183ef13e51
Андрей Епифанов <epifanovmd@gmail.com>
2024-02-16 02:57:30 +0300

    refactoring

--- src/hooks/useInitialize.ts
+++ src/hooks/useInitialize.ts
@@ -1,20 +1,5 @@
-import {
-  InitializeDispose,
-  isArray,
-  isFunction,
-  SupportInitialize,
-} from '@force-dev/utils';
+import { SupportInitialize, disposer } from '@force-dev/utils';
 import { useEffect, useRef } from 'react';
-
-const disposer = (dispose: Exclude<InitializeDispose, Promise<any>>) => {
-  if (isArray(dispose)) {
-    dispose.forEach(disposer);
-  }
-
-  if (isFunction(dispose)) {
-    dispose();
-  }
-};
 
 export const useInitialize = <D, T>(
   vm: T & SupportInitialize<D>,
@@ -25,35 +10,14 @@ export const useInitialize = <D, T>(
 
   useEffect(() => {
     isInitialized.current = true;
-    const disposes: (() => void)[] = [];
 
     if (initialize) {
       const dispose = initialize(rest?.[0]);
 
-      if (dispose instanceof Promise) {
-        dispose
-          .then(disposable => {
-            if (isFunction(disposable)) {
-              disposes.push(disposable);
-            }
-
-            if (isArray(disposable)) {
-              disposable.forEach(d => {
-                disposes.push(d);
-              });
-            }
-          })
-          .catch(() => {});
-      } else {
-        return () => {
-          disposer(dispose);
-        };
-      }
+      return () => {
+        disposer(dispose);
+      };
     }
-
-    return () => {
-      disposer(disposes);
-    };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
