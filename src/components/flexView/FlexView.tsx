0fa2b47ed59496e0eac7cb98a5a51b19151b2218
Андрей Епифанов <epifanovmd@gmail.com>
2024-02-24 22:11:43 +0300

    refactoring

--- src/components/flexView/FlexView.tsx
+++ src/components/flexView/FlexView.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react';
-import { FC, memo, PropsWithChildren } from 'react';
+import { forwardRef, memo, PropsWithChildren } from 'react';
 import { Animated, View, ViewProps } from 'react-native';
 import { useFlexProps } from './useFlexProps';
 import { FlexProps } from './types';
 
 type Props = FlexProps & ViewProps;
 
-export const FlexView: FC<PropsWithChildren<Props>> = memo(props => {
-  const { ownProps, style, animated } = useFlexProps(props);
+export const FlexView = memo(
+  forwardRef<View, PropsWithChildren<Props>>((props, ref) => {
+    const { ownProps, style, animated } = useFlexProps(props);
 
-  const Component = animated ? Animated.View : View;
+    const Component = animated ? Animated.View : View;
 
-  return <Component style={style} {...ownProps} />;
-});
+    return <Component ref={ref} style={style} {...ownProps} />;
+  }),
+);
 
-export const Col: FC<PropsWithChildren<Props>> = memo(props => (
-  <FlexView col={true} {...props} />
-));
-export const Row: FC<PropsWithChildren<Props>> = memo(props => (
-  <FlexView row={true} {...props} />
-));
+export const Col = memo(
+  forwardRef<View, PropsWithChildren<Props>>((props, ref) => (
+    <FlexView ref={ref} col={true} {...props} />
+  )),
+);
+export const Row = memo(
+  forwardRef<View, PropsWithChildren<Props>>((props, ref) => (
+    <FlexView ref={ref} row={true} {...props} />
+  )),
+);
