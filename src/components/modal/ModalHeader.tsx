545fa02649794c2a71ea193074c83a31d363043d
Андрей Епифанов <epifanovmd@gmail.com>
2024-03-13 22:23:23 +0300

    fix modal header

--- src/components/modal/ModalHeader.tsx
+++ src/components/modal/ModalHeader.tsx
@@ -33,7 +33,7 @@ export const ModalHeader: FC<PropsWithChildren<ModalHeaderProps>> = ({
   iconColor,
   color,
   renderCloseIcon = (fill?: ColorValue) => (
-    <Svg viewBox="0 0 24 24" fill={fill}>
+    <Svg viewBox="0 0 24 24" height={24} width={24} fill={fill}>
       <Path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" />
     </Svg>
   ),
