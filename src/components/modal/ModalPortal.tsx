2e56037d941711227184a97060b9eee3b5cd1d16
Андрей Епифанов <epifanovmd@gmail.com>
2024-03-26 18:08:01 +0300

    refactoring

--- src/components/modal/ModalPortal.tsx
+++ src/components/modal/ModalPortal.tsx
@@ -1,8 +1,8 @@
 import { Portal } from '@gorhom/portal';
 import * as React from 'react';
 import { memo, PropsWithChildren } from 'react';
-import { uuid } from '@force-dev/utils';
+import { shortid } from '@force-dev/utils';
 
 export const ModalPortal = memo(({ children }: PropsWithChildren) => (
-  <Portal key={`modal-port_${uuid()}`}>{children}</Portal>
+  <Portal key={`modal-port_${shortid()}`}>{children}</Portal>
 ));
