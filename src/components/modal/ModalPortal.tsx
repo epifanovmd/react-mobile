690e3c996c32f604fa62240a3d8b7394e39fb0c8
Андрей Епифанов <epifanovmd@gmail.com>
2024-06-19 14:58:48 +0300

    fix errors

--- src/components/modal/ModalPortal.tsx
+++ src/components/modal/ModalPortal.tsx
@@ -1,7 +1,7 @@
-import { shortid } from "@force-dev/utils";
 import { Portal } from "@gorhom/portal";
 import * as React from "react";
 import { memo, PropsWithChildren } from "react";
+import shortid from "shortid";
 
 export const ModalPortal = memo(({ children }: PropsWithChildren) => (
   <Portal key={`modal-port_${shortid()}`}>{children}</Portal>
